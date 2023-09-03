76c196d496bab1da3b094b128d38070e41125b95
Dmytro <moiseenkodmitriy@i.ua>
2023-09-03 21:56:44 +0300

    deploy

--- src/js/utils/params.js
+++ src/js/utils/params.js
@@ -1,15 +1,14 @@
 const DEBOUNCE_DELAY = 1200;
-export const LIGHTBOX_PARAMS = {
-  captionsData: 'alt',
-  captionDelay: 250,
-  scrollZoom: false,
-};
+// export const LIGHTBOX_PARAMS = {
+//   captionsData: 'alt',
+//   captionDelay: 250,
+//   scrollZoom: false,
+// };
 let nameCountry = '';
 let nameCapital = '';
 
 export default {
   DEBOUNCE_DELAY,
-
   nameCountry,
   nameCapital,
 };
