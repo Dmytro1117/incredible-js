f3b42541f0b06c2d253d0e0ab636807d6b5b15c0
Dmytro <moiseenkodmitriy@i.ua>
2023-09-03 21:21:45 +0300

    dfdf

--- src/js/utils/params.js
+++ src/js/utils/params.js
@@ -1,15 +1,11 @@
 const DEBOUNCE_DELAY = 1200;
-LIGHTBOX_PARAMS = {
-  captionsData: 'alt',
-  captionDelay: 250,
-  scrollZoom: false,
-};
+
 let nameCountry = '';
 let nameCapital = '';
 
 export default {
   DEBOUNCE_DELAY,
-  LIGHTBOX_PARAMS,
+
   nameCountry,
   nameCapital,
 };
